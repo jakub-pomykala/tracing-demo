9290f7985d0f146f6169b12949bce2a953c5f01e
koyfman <koyfman@us.ibm.com>
2019-03-26 00:16:01 -0400

    Add images with tracing disabed

---
+++ rmdeps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl delete deployment instrument-craft-shop
+kubectl delete deployment maker-bot
+kubectl delete deployment dbwrapper
+kubectl delete deployment pipeline-n1
+kubectl delete deployment pipeline-n2
+kubectl delete deployment pipeline-n3
+
+
